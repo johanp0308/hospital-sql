411e4914f80b728acb26f11143790bd1f78a7f65
johanp0308 <111256873+johanp0308@users.noreply.github.com>
2023-11-23 02:26:00 -0500

    feat: :sparkles: Add Column to 'empleado'

--- hospitalModelo.sql
+++ hospitalModelo.sql
@@ -28,7 +28,8 @@ CREATE TABLE habitacion(
 CREATE TABLE empleado(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(30) NOT NULL,
-    cargo VARCHAR(30) NOT NULL
+    cargo VARCHAR(30) NOT NULL,
+    id_piso VARCHAR(30) NULL
 );
 CREATE TABLE complejo_hospitalario(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -54,6 +55,3 @@ ALTER TABLE piso ADD CONSTRAINT piso_id_edificio_foreign FOREIGN KEY(id_edificio
 ALTER TABLE edificio ADD CONSTRAINT edificio_id_complejo_foreign FOREIGN KEY(id_complejo) REFERENCES complejo_hospitalario(id);
 ALTER TABLE piso_especialidad ADD CONSTRAINT piso_especialidad_id_piso_foreign FOREIGN KEY(id_piso) REFERENCES piso(id_piso_edificio);
 ALTER TABLE piso ADD CONSTRAINT piso_id_empleado_foreign FOREIGN KEY(id_empleado) REFERENCES empleado(id);
-
---  
-
